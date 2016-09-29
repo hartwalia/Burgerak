e3670c8961535da2ae74e6e63c8428f37c26d312
Hart <Hart>
2016-09-29 16:03:25 +0800

    First commit

---
+++ app/src/main/java/com/example/hart/burgerak/HomeFragment.java
@@ -0,0 +1,56 @@
+package com.example.hart.burgerak;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    private EditText mSearchInputEditText;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mSearchInputEditText = (EditText) view.findViewById(R.id.fragment_home_search_input);
+        ViewCompat.setElevation(mSearchInputEditText, 8);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mSearchInputEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Log.d("received", mSearchInputEditText.getText().toString());
+
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.activity_main_rl_container, new ListFragment())
+                            .addToBackStack("ListFragment")
+                            .commit();
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/hart/burgerak/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hart.burgerak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.activity_main_rl_container, new HomeFragment())
+                .commit();
+    }
+}
