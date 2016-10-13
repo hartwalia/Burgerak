944ab3391edb6c38e77f28bcfcd3955a8b43a3ec
Hart <Hart>
2016-10-13 17:28:26 +0800

    updated User model

--- app/src/main/java/com/example/hart/burgerak/model/User.java
+++ app/src/main/java/com/example/hart/burgerak/model/User.java
@@ -1,5 +1,9 @@
 package com.example.hart.burgerak.model;
 
+import android.support.annotation.NonNull;
+
+import org.json.JSONObject;
+
 /**
  * Created by 3SHEN on 1/10/2016.
  */
@@ -7,29 +11,42 @@ package com.example.hart.burgerak.model;
 public class User {
     private String mName;
     private String mEmail;
-    private int mContactNo;
+    private String mId;
+    private String mUserToken;
 
-    public String getName() {
-        return mName;
+    public User(@NonNull JSONObject jsonObject) {
+        /*
+        {
+        	"userStatus": "ENABLED",
+	        "created": 1475211805000,
+	        "name": null,
+	        "ownerId": "CC06DDDF-4E42-3B3A-FF73-55EA7886D900",
+	        "updated": null,
+	        "email": "demo1@apptivitylab.com",
+	        "objectId": "CC06DDDF-4E42-3B3A-FF73-55EA7886D900",
+	        "__meta": "{\"relationRemovalIds\":{},\"selectedProperties\":[\"password\",\"created\",\"___saved\",\"___class\",\"name\",\"ownerId\",\"updated\",\"email\",\"objectId\"],\"relatedObjects\":{}}"
+        }
+         */
+
+        mId = jsonObject.optString("ownerId");
+        mEmail = jsonObject.optString("email");
+        mName = jsonObject.optString("name");
+        mUserToken = jsonObject.optString("user-token");
     }
 
-    public void setName(String name) {
-        mName = name;
+    public String getId() {
+        return mId;
     }
 
     public String getEmail() {
         return mEmail;
     }
 
-    public void setEmail(String email) {
-        mEmail = email;
+    public String getName() {
+        return mName;
     }
 
-    public int getContactNo() {
-        return mContactNo;
-    }
-
-    public void setContactNo(int contactNo) {
-        mContactNo = contactNo;
+    public String getUserToken() {
+        return mUserToken;
     }
 }
